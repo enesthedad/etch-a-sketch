3596df5d0f1ea71890edcfde64d23e310ee82f1e
enesthedad <34135795+enesthedad@users.noreply.github.com>
2023-07-23 13:42:04 +0300

    Styled with new colors Add custom grid

--- logic.js
+++ logic.js
@@ -1,29 +1,80 @@
+const btns = document.querySelectorAll('.btn');
+const resetBtn = document.querySelector('#reset');
+
+const eraserBtn=document.getElementById('eraser');
+const blackBtn=document.getElementById('black');
+const rgbBtn=document.getElementById('rgb');
 const box = document.getElementById('sketch');
-for(let i=0;i<64*64;i++){
-    let el = document.createElement('div');
-    el.id='b'+i;
-    el.className='box';
+let currentColor = 'black';
+const grid16=document.getElementById('16');
+const grid32=document.getElementById('32');
+const grid48=document.getElementById('48');
+const grid64=document.getElementById('64');
 
-    box.appendChild(el);
 
-}
+let grid = 64;
+
+
+
+    for(let i=0;i<64*64;i++){
+        const el = document.createElement('div');
+        el.id='b'+i;
+        el.className='box';
+    
+        box.appendChild(el);
+    
+    }
+
+
+
 const randomHex=function(){
     return Math.trunc(Math.random()*255)+1;
 }
 
+resetBtn.addEventListener('click',function(){
+    colored.forEach(el=>el.style.backgroundColor='white');
+    currentColor='black';
+    btns.forEach((but)=>but.classList.remove('active'));
+    blackBtn.classList.add('active');
+})
 const colored = document.querySelectorAll('.box');
+blackBtn.addEventListener('click',function(){ 
+    currentColor='black';
+    btns.forEach((but)=>but.classList.remove('active'));
+    blackBtn.classList.add('active');
+});
+eraserBtn.addEventListener('click',function(){
+    currentColor='white'
+    btns.forEach((but)=>but.classList.remove('active'));
+    eraserBtn.classList.add('active');
+});
+rgbBtn.addEventListener('click',function() {
+    currentColor='rgb';
+    btns.forEach((but)=>but.classList.remove('active'));
+    rgbBtn.classList.add('active');
+});
 
 
 const paintEle = function(boxi){
-    boxi.onmousedown = ()=> console.log('1');
+    
     boxi.addEventListener('mouseover',function(eve){
+        if(currentColor=='black'){
+        boxi.style.backgroundColor=`black`;
+        
+        }else if(currentColor == 'white'){
+        boxi.style.backgroundColor=`white`;
+       
+        }else if(currentColor=='rgb'){
         boxi.style.backgroundColor=`rgb(${randomHex()},${randomHex()},${randomHex()})`;
+        
+        }
+        
+        
     })
 }
-colored.forEach(ell =>paintEle(ell));
-const btn = document.querySelector('#reset');
 
-btn.addEventListener('click',function(){
-    colored.forEach(el=>el.style.backgroundColor='white')
-})
+
+
+colored.forEach(ell =>paintEle(ell));
+
 
